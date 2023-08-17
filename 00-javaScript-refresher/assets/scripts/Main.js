e6df3f86710b24082deea877b66e207bbcac94c4
ManviSekhri <manvisekhri08@gmail.com>
2023-08-17 18:19:52 +0530

    classes and objects

--- 00-javaScript-refresher/assets/scripts/Main.js
+++ 00-javaScript-refresher/assets/scripts/Main.js
@@ -1,5 +1,5 @@
 //import { apiKey, abc } from "./util.js";
-
+// for import and export keywords type = module necessary in script tag
 //import apiKey from "./util.js"; //default
 
 import * as util from "./util.js";   // grouping multiple keys in one object
@@ -12,3 +12,32 @@ console.log(util.default);
 // import { apiKey, abc as content } from "./util.js";  // if i don't like the name abc i can change it here
 
 // console.log(content);
+
+// Objects and classes
+const user = {
+    name: "Hi, Its me",
+    age: 33,
+    greet() {
+        console.log("I'm the problem !");
+        console.log(this.age);
+    }
+};
+console.log(user);
+console.log(user.name);
+console.log(user.age);
+user.greet();
+
+
+class Users {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    greet() {
+        console.log("I'm the problem !");
+    }
+}
+
+const user1 = new Users("Lana", 37)
+console.log(user1)
+user1.greet();
